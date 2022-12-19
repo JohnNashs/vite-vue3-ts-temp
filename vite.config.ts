b0bf2158dd0a89938bb3fb453597748cd637b68a
YuminFu <923410813@qq.com>
2022-12-19 21:22:18 +0800

    feat:config windicss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import postcsspxtoviewport from "postcss-px-to-viewport"
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 8080
+  },
+  plugins: [vue(), WindiCSS()],
   css: {
     postcss: {
       plugins: [
