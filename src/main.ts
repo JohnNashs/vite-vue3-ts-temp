6734b579ab949987244effdce7b44b79449b1413
YuminFu <923410813@qq.com>
2022-12-20 22:07:12 +0800

    feat: add lodash

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import _ from 'lodash';
+console.log(_.chunk(['a', 'b', 'c', 'd'], 2));
 
 
 createApp(App).mount('#app')
